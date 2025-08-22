4d19045d3334971f6a9d7fabdafba38c130722a3
Bogi168 <danielbogner5@gmail.com>
2025-08-22 22:59:33 +0200

    Update _player.py

--- _player.py
+++ _player.py
@@ -1,4 +1,4 @@
-go_home_now = []
+# Class Player
 class Player:
     def __init__(self, player_name, inside = True, pocket = 0, chest = 0, is_bot = False):
         self.player_name = player_name
@@ -7,67 +7,78 @@ class Player:
         self.chest = chest
         self.is_bot = is_bot
 
-    def go_home(self):
+    def go_home(self, e_go_home_now):
         self.chest += self.pocket
         self.pocket = 0
-        go_home_now.append(self)
+        e_go_home_now.append(self)
         self.inside = False
+        return e_go_home_now
 
     def die(self):
         self.pocket = 0
         self.inside = False
 
-    def tell_p_pocket(self):
+    def tell_p_diamonds(self, diamonds_on_way):
         print(f"You have {self.pocket} diamond(s) in your pocket")
-
-    def ask_player(self, diamonds_on_way):
-        self.tell_p_pocket()
         print(f"There is/are {diamonds_on_way} diamond(s) on the way home")
         print("_____________________________________________________________")
+
+    def ask_player(self, diamonds_on_way, e_go_home_now):
+        self.tell_p_diamonds(diamonds_on_way)
         question = input(f"{self.player_name}: Do you want to go home? (Y/N) ").upper()
         while not question.isalpha():
             question = input(f"{self.player_name}: {question} is not valid. Do you want to go home? (Y/N) ").upper()
         print("_____________________________________________________________")
         if question == "Y":
-            self.go_home()
+            self.go_home(e_go_home_now)
 
-    def ask_bot(self, diamonds_on_way, relics_on_way, probability):
+    def ask_bot(self, diamonds_on_way, relics_on_way, probability, e_go_home_now):
         pass
 
+# Class Bot
 class Bot(Player):
     def __init__(self, bot_name, level):
         super().__init__(player_name = bot_name, is_bot = True)
         self.level = level
         self.bot_name = bot_name
 
-    def tell_b_pocket(self):
+    def tell_b_diamonds(self, diamonds_on_way):
         print(f"{self.bot_name} has {self.pocket} diamond(s) in his pocket")
-
-    def ask_bot(self, diamonds_on_way, relics_on_way, probability):
-        self.tell_b_pocket()
         print(f"There is/are {diamonds_on_way} diamond(s) on the way home")
         print("_____________________________________________________________")
+
+    def bot_goes_home(self, e_go_home_now):
+        self.go_home(e_go_home_now)
+        print(f"{self.bot_name} goes home and saves his diamond(s)")
+        print("_____________________________________________________________")
+
+    def bot_stays_inside(self):
+        print(f"{self.bot_name} stays inside")
+        print("_____________________________________________________________")
+
+    def bot_lvl1(self, diamonds_on_way, relics_on_way, probability, e_go_home_now):
         if self.level == 1:
             if probability > 0.1 and (diamonds_on_way + relics_on_way + self.pocket) != 0:
-                self.go_home()
-                print(f"{self.bot_name} goes home and saves his diamond(s)")
-                print("_____________________________________________________________")
+                self.bot_goes_home(e_go_home_now)
             else:
-                print(f"{self.bot_name} stays inside")
-                print("_____________________________________________________________")
-        elif self.level == 2:
+                self.bot_stays_inside()
+
+    def bot_lvl2(self, diamonds_on_way, relics_on_way, probability, e_go_home_now):
+        if self.level == 2:
             if probability > 0.17 and (diamonds_on_way + relics_on_way + self.pocket) != 0:
-                self.go_home()
-                print(f"{self.bot_name} goes home and saves his diamond(s)")
-                print("_____________________________________________________________")
+                self.bot_goes_home(e_go_home_now)
             else:
-                print(f"{self.bot_name} stays inside")
-                print("_____________________________________________________________")
-        elif self.level == 3:
+                self.bot_stays_inside()
+
+    def bot_lvl3(self, diamonds_on_way, relics_on_way, probability, e_go_home_now):
+        if self.level == 3:
             if probability > 0.25 and (diamonds_on_way + relics_on_way + self.pocket) != 0:
-                self.go_home()
-                print(f"{self.bot_name} goes home and saves his diamond(s)")
-                print("_____________________________________________________________")
+                self.bot_goes_home(e_go_home_now)
             else:
-                print(f"{self.bot_name} stays inside")
-                print("_____________________________________________________________")
+                self.bot_stays_inside()
+
+    def ask_bot(self, diamonds_on_way, relics_on_way, probability, e_go_home_now):
+        self.tell_b_diamonds(diamonds_on_way)
+        self.bot_lvl1(diamonds_on_way, relics_on_way, probability, e_go_home_now)
+        self.bot_lvl2(diamonds_on_way, relics_on_way, probability, e_go_home_now)
+        self.bot_lvl3(diamonds_on_way, relics_on_way, probability, e_go_home_now)
