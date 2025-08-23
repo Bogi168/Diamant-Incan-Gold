64ddaf4f88159ee2af770d876b0c0beda6977342
Bogi168 <danielbogner5@gmail.com>
2025-08-23 03:28:41 +0200

    Add files via upload

---
+++ _cards.py
@@ -0,0 +1,35 @@
+import random
+
+class Cards:
+    def __init__(self):
+        # Define cards
+        self.snakes = ["🐍"] * 3
+        self.spiders = ["🕷"] * 3
+        self.fires = ["🔥"] * 3
+        self.avalanches = ["🌑"] * 3
+        self.mummys = ["👤"] * 3
+        self.treasure_cards = [1, 2, 3, 4, 5, 5, 7, 7, 9, 11, 11, 13, 14, 15, 17]
+        self.relics = [5.01, 7.01, 8.01, 10.01, 12.01]
+        self.traps = self.snakes + self.spiders + self.fires + self.avalanches + self.mummys
+        self.full_deck = self.traps + self.treasure_cards
+
+        # Define card lists
+        self.deck = []
+        self.new_card = None
+        self.played_cards = []
+        self.p_relics = self.relics.copy()
+
+    # Reset played cards
+    def reset_played_cards(self):
+        self.played_cards.clear()
+        self.deck = self.full_deck.copy()
+
+    # Draw cards
+    def draw_card(self):
+        self.new_card = random.choice(self.deck)
+        self.deck.remove(self.new_card)
+
+    def add_relics(self, round_num):
+        relic = self.p_relics[round_num]
+        self.full_deck.append(relic)
+        self.deck = self.full_deck.copy()
