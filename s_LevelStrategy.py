91aa8d9eba19a04586a705f46c3f62587fbc9942
Bogi168 <danielbogner5@gmail.com>
2026-02-07 22:52:17 +0100

    Update s_LevelStrategy.py

--- s_LevelStrategy.py
+++ s_LevelStrategy.py
@@ -1,53 +1,53 @@
-from Main_Game.LevelStrategy import *
+from Main_Game.m_LevelStrategy import *
 
 class s_Level_1(Level_1):
     def action(self):
 #        if self.game_object.dying_prob > 0.08 and self.bool_diamonds_available():
-#            self.current_bot.bot_goes_home()
+#            self.current_bot.go_home()
 
         if ((self.game_object.calc_ev_next_dia_on_way(self.current_bot) - self.current_bot.guaranteed_diamonds) < 0
                 and self.bool_diamonds_available()):
-            self.current_bot.bot_goes_home()
+            self.current_bot.go_home()
 
 
 #        if (self.game_object.calc_ev_next(self.current_bot) - self.current_bot.pocket) < 0 and self.bool_diamonds_available():
-#            self.current_bot.bot_goes_home()
+#            self.current_bot.go_home()
 
 #        elif self.game_object.calc_undiscovered_diamonds() < 50:
-#            self.current_bot.bot_goes_home()
+#            self.current_bot.go_home()
 
 #        elif len(self.game_object.players_inside) != 1:
 #            if self.amount_available_diamonds / (len(self.game_object.players_inside) - 1) > 12 and self.game_object.calc_prob() > 0:
-#                self.current_bot.bot_goes_home()
+#                self.current_bot.go_home()
 
 class s_Level_2(Level_2):
     def action(self):
-        if self.game_object.dying_prob > 0.17 and self.bool_diamonds_available():
-            self.current_bot.bot_goes_home()
+        if self.game_object.dying_prob > 0.08 and self.bool_diamonds_available():
+            self.current_bot.go_home()
 
 #        if (self.game_object.calc_ev_next(self.current_bot) - self.current_bot.pocket) < 0 and self.bool_diamonds_available():
-#            self.current_bot.bot_goes_home()
+#            self.current_bot.go_home()
 
 #        elif self.game_object.calc_undiscovered_diamonds() < 70:
-#            self.current_bot.bot_goes_home()
+#            self.current_bot.go_home()
 
 #        elif len(self.game_object.players_inside) != 1:
 #            if self.amount_available_diamonds / (len(self.game_object.players_inside) - 1) > 16 and self.game_object.calc_prob() > 0.09:
-#                self.current_bot.bot_goes_home()
+#                self.current_bot.go_home()
 
 class s_Level_3(Level_3):
     def action(self):
         if self.game_object.dying_prob > 0.25 and self.bool_diamonds_available():
-                self.current_bot.bot_goes_home()
+                self.current_bot.go_home()
 
 #        if (self.game_object.calc_ev_next(self.current_bot) - self.current_bot.pocket) < 0 and self.bool_diamonds_available():
-#            self.current_bot.bot_goes_home()
+#            self.current_bot.go_home()
 
 #        elif self.game_object.calc_undiscovered_diamonds() < 30:
-#            self.current_bot.bot_goes_home()
+#            self.current_bot.go_home()
 
 #        elif self.amount_available_diamonds / len(self.game_object.players_inside) > 16 and len(self.game_object.players_inside) != 1 and self.game_object.calc_prob() > 0.15:
-#            self.current_bot.bot_goes_home()
+#            self.current_bot.go_home()
 
 class s_Act_On_Card(Act_On_Card):
     def __init__(self, current_bot, game_object):
