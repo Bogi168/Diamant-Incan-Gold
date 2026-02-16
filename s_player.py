d8f51164c3d85cfa04c8d667cb4e3f38672a68f1
Bogi168 <danielbogner5@gmail.com>
2026-02-16 18:52:36 +0100

    Update s_player.py

--- s_player.py
+++ s_player.py
@@ -5,6 +5,7 @@ class s_Bot(Bot):
         super().__init__(bot_name = bot_name, level = level,  game_object = game_object)
         self.game_winning_count = 0
         self.round_winning_count = 0
+        self.die_counter = 0
         self.diamond_count = 0
         self.collected_diamonds = []
         self.max_diamonds = 0
