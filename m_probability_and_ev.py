c96716408bffbfd750ac6b93217d8e2493d5b4cd
Bogi168 <danielbogner5@gmail.com>
2026-02-16 18:50:13 +0100

    Update m_probability_and_ev.py

--- m_probability_and_ev.py
+++ m_probability_and_ev.py
@@ -1,48 +1,44 @@
-class Probability_and_EV:
-    def __init__(self, game_object):
-        self.game_object = game_object
+# Calculate probability of dying on the next move
+def calc_dying_prob(game_object, cards_object):
+    killing_traps = 0
+    probability = 0
+    cards_object.played_cards.append(cards_object.new_card)
+    for card in cards_object.played_cards:
+        if card.card_type == "trap":
+            traps_in_game = cards_object.deck.count(card)
+            killing_traps += traps_in_game
+    probability += killing_traps / len(cards_object.deck)
+    cards_object.played_cards.pop(-1)
+    game_object.dying_prob = probability
 
-    # Calculate probability of dying on the next move
-    def calc_dying_prob(self):
-        killing_traps = 0
-        probability = 0
-        self.game_object.cards.played_cards.append(self.game_object.cards.new_card)
-        for card in self.game_object.cards.played_cards:
-            if card in self.game_object.cards.traps:
-                traps_in_game = self.game_object.cards.deck.count(card)
-                killing_traps += traps_in_game
-        probability += killing_traps / len(self.game_object.cards.deck)
-        self.game_object.cards.played_cards.pop(-1)
-        self.game_object.dying_prob = probability
+# Calculate amount of undiscovered diamonds
+def calc_undiscovered_diamonds(game_object, cards_object):
+    game_object.undiscovered_diamonds = 0
+    cards_object.played_cards.append(cards_object.new_card)
+    for card in cards_object.deck:
+        if card.card_type == "treasure_card":
+            game_object.undiscovered_diamonds += card.value
+    cards_object.played_cards.pop(-1)
 
-    # Calculate amount of undiscovered diamonds
-    def calc_undiscovered_diamonds(self):
-        self.game_object.undiscovered_diamonds = 0
-        self.game_object.cards.played_cards.append(self.game_object.cards.new_card)
-        for card in self.game_object.cards.deck:
-            if card.card_type == "treasure_card":
-                self.game_object.undiscovered_diamonds += card.value
-        self.game_object.cards.played_cards.pop(-1)
+def calc_guaranteed_diamonds(game_object, current_player):
+    current_player.guaranteed_diamonds = current_player.pocket + game_object.diamonds_on_way // len(game_object.players_inside)
+    if len(game_object.players_inside) == 1:
+        current_player.guaranteed_diamonds += game_object.relics_on_way
 
-    def calc_guaranteed_diamonds(self, player):
-        player.guaranteed_diamonds = player.pocket + self.game_object.diamonds_on_way // len(self.game_object.players_inside)
-        if len(self.game_object.players_inside) == 1:
-            player.guaranteed_diamonds += self.game_object.relics_on_way
+def calc_ev_next(game_object, cards_object, current_player):
+    calc_dying_prob(game_object = game_object, cards_object = cards_object)
+    game_object.surviving_prob = (1 - game_object.dying_prob)
+    calc_undiscovered_diamonds(game_object = game_object, cards_object = cards_object)
+    future_diamonds = (game_object.undiscovered_diamonds / (len(cards_object.deck) * len(game_object.players_inside))
+                       + current_player.pocket)
+    ev_next = game_object.surviving_prob * future_diamonds - game_object.dying_prob * current_player.pocket
+    return ev_next
 
-    def calc_ev_next(self, player):
-        self.calc_dying_prob()
-        self.game_object.surviving_prob = (1 - self.game_object.dying_prob)
-        self.calc_undiscovered_diamonds()
-        future_diamonds = (self.game_object.undiscovered_diamonds / (len(self.game_object.cards.deck) * len(self.game_object.players_inside))
-                           + player.pocket) #+ self.diamonds_on_way // len(self.players_inside))
-        ev_next = self.game_object.surviving_prob * future_diamonds - self.game_object.dying_prob * (player.pocket) #+ self.diamonds_on_way // len(self.players_inside))
-        return ev_next
-
-    def calc_ev_next_dia_on_way(self, player):
-        self.calc_dying_prob()
-        self.game_object.surviving_prob = (1 - self.game_object.dying_prob)
-        self.calc_undiscovered_diamonds()
-        self.calc_guaranteed_diamonds(player)
-        future_diamonds = self.game_object.undiscovered_diamonds / (len(self.game_object.cards.deck) * len(self.game_object.players_inside)) + player.guaranteed_diamonds
-        ev_next = self.game_object.surviving_prob * future_diamonds - self.game_object.dying_prob * player.guaranteed_diamonds
-        return ev_next
+def calc_ev_next_dia_on_way(game_object, cards_object, current_player):
+    calc_dying_prob(game_object = game_object, cards_object = cards_object)
+    game_object.surviving_prob = (1 - game_object.dying_prob)
+    calc_undiscovered_diamonds(game_object = game_object, cards_object = cards_object)
+    calc_guaranteed_diamonds(game_object = game_object, current_player = current_player)
+    future_diamonds = game_object.undiscovered_diamonds / (len(game_object.cards.deck) * len(game_object.players_inside)) + current_player.guaranteed_diamonds
+    ev_next = game_object.surviving_prob * future_diamonds - game_object.dying_prob * current_player.guaranteed_diamonds
+    return ev_next
