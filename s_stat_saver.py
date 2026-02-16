59e979c2f005475822aa789eaef7be10427677c1
Bogi168 <danielbogner5@gmail.com>
2026-02-16 18:53:05 +0100

    Update s_stat_saver.py

--- s_stat_saver.py
+++ s_stat_saver.py
@@ -1,11 +1,10 @@
-file_path = "s_stats.txt"
-
-def save_statistics(game_object):
+def save_statistics(game_object, file_path):
     with open(file_path,"a") as file:
         game_object.get_max_diamonds()
-        for e in game_object.explorers:
-            file.write("\n" + "*"*46 + "\n"
-                       f"Stats for Bot Level {e.level}: \n"
-                       f"The Bot collects {((e.diamond_count / 5) / game_object.games_amount):.1f} diamonds per round \n"
-                       f"He collected {e.max_diamonds} diamonds in his best round \n" +
-                       "*"*46 + "\n")
+        for explorer in game_object.explorers:
+            file.write("\n" + "*" * 60 + "\n"
+                       f"Stats for bot level {explorer.level}: \n"
+                       f"The bot collects {((explorer.diamond_count / 5) / game_object.games_amount):.1f} diamonds per round \n"
+                       f"He collected {explorer.max_diamonds} diamonds in his best round \n"
+                       f"The bot died in {(explorer.die_counter / (game_object.games_amount * 5)) * 100:.2f}% of the rounds \n" +
+                       "*" * 60 + "\n")
