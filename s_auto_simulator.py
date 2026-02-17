a0ffd419995a9aeea770ccdb733fde9996719736
Bogi168 <danielbogner5@gmail.com>
2026-02-17 16:39:16 +0100

    Update s_auto_simulator.py

--- s_auto_simulator.py
+++ s_auto_simulator.py
@@ -1,7 +1,7 @@
-from s_stat_saver import save_statistics
-from s_console import console_select_games_amount
-from s_player import s_Bot
-from s_game import s_Game
+from Simulation.s_stat_saver import save_statistics
+from Simulation.s_renders import render_select_games_amount
+from Simulation.s_player import s_Bot
+from Simulation.s_game import s_Game
 
 file_path = "s_auto_simulation"
 
@@ -15,7 +15,7 @@ class s_Auto_Game(s_Game):
             s_Bot(bot_name=f"Bot {self.level_bot}", level = self.level_bot, game_object = self))
 
     def main(self):
-        console_select_games_amount(game_object = self)
+        render_select_games_amount(game_object = self)
         for self.level_bot in range(1, 14):
             self.explorers.clear()
             self.create_bot()
