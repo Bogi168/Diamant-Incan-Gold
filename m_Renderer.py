8a7188b81329e2203f924ede9ddcb99fe8ce9222
Bogi168 <danielbogner5@gmail.com>
2026-02-17 04:15:03 +0100

    Add files via upload

---
+++ m_Renderer.py
@@ -0,0 +1,80 @@
+from abc import ABC, abstractmethod
+
+class Renderer(ABC):
+    @abstractmethod
+    def render(self, message):
+        pass
+
+    @abstractmethod
+    def ask_players_amount(self):
+        pass
+
+    @abstractmethod
+    def re_ask_players_amount(self):
+        pass
+
+    @abstractmethod
+    def ask_player_name(self, player_num):
+        pass
+
+    @abstractmethod
+    def ask_bots_amount(self):
+        pass
+
+    @abstractmethod
+    def re_ask_bots_amount(self):
+        pass
+
+    @abstractmethod
+    def ask_bot_level(self, bot_num):
+        pass
+
+    @abstractmethod
+    def re_ask_bot_level(self, bot_num):
+        pass
+
+    @abstractmethod
+    def ask_continue_or_leave(self, player_name):
+        pass
+
+    @abstractmethod
+    def re_ask_continue_or_leave(self, player_name):
+        pass
+
+    @abstractmethod
+    def ask_again(self):
+        pass
+
+class ConsoleRenderer(Renderer):
+    def render(self, message):
+        print(message)
+
+    def ask_players_amount(self):
+        return input("How many players?: ")
+
+    def re_ask_players_amount(self):
+        return input("That's not a valid number. How many players? ")
+
+    def ask_player_name(self, player_num):
+        return input(f"Enter player number {player_num + 1}'s name: ").capitalize()
+
+    def ask_bots_amount(self):
+        return input("\nHow many bots? ")
+
+    def re_ask_bots_amount(self):
+        return input("That's not a valid number. How many bots? ")
+
+    def ask_bot_level(self, bot_num):
+        return input(f"Select a level for Bot {bot_num + 1} (careful: 1 / medium: 2 / risky: 3): ")
+
+    def re_ask_bot_level(self, bot_num):
+        return input(f"That's not a valid level. Select a level for Bot {bot_num + 1} (careful: 1 / medium: 2 / risky: 3): ")
+
+    def ask_continue_or_leave(self, player_name):
+        return input(f"{player_name}: Do you want to go home? (Y/N) ").upper()
+
+    def re_ask_continue_or_leave(self, player_name):
+        return input(f"{player_name}: That's is not valid. Do you want to go home? (Y/N) ").upper()
+
+    def ask_again(self):
+        return input("Do you want to play again? (Y/N) ").upper()
