703e4a310da189e5497f5267d8018170d4d7eec6
Bogi168 <danielbogner5@gmail.com>
2026-02-17 16:35:09 +0100

    Update m_Renderer.py

--- m_Renderer.py
+++ m_Renderer.py
@@ -34,6 +34,14 @@ class Renderer(ABC):
         pass
 
     @abstractmethod
+    def ask_games_amount(self):
+        pass
+
+    @abstractmethod
+    def re_ask_games_amount(self):
+        pass
+
+    @abstractmethod
     def ask_continue_or_leave(self, player_name):
         pass
 
@@ -43,6 +51,10 @@ class Renderer(ABC):
 
     @abstractmethod
     def ask_again(self):
+        pass
+
+    @abstractmethod
+    def ask_for_save(self):
         pass
 
 class ConsoleRenderer(Renderer):
@@ -70,6 +82,12 @@ class ConsoleRenderer(Renderer):
     def re_ask_bot_level(self, bot_num):
         return input(f"That's not a valid level. Select a level for Bot {bot_num + 1} (careful: 1 / medium: 2 / risky: 3): ")
 
+    def ask_games_amount(self):
+        pass
+
+    def re_ask_games_amount(self):
+        pass
+
     def ask_continue_or_leave(self, player_name):
         return input(f"{player_name}: Do you want to go home? (Y/N) ").upper()
 
@@ -78,3 +96,49 @@ class ConsoleRenderer(Renderer):
 
     def ask_again(self):
         return input("Do you want to play again? (Y/N) ").upper()
+
+    def ask_for_save(self):
+        pass
+
+class SimulationRenderer(Renderer):
+    def render(self, message):
+        print(message)
+
+    def ask_players_amount(self):
+        pass
+
+    def re_ask_players_amount(self):
+        pass
+
+    def ask_player_name(self, player_num):
+        pass
+
+    def ask_bots_amount(self):
+        return input("\nHow many bots? ")
+
+    def re_ask_bots_amount(self):
+        return input("That's not a valid number. How many bots? ")
+
+    def ask_bot_level(self, bot_num):
+        return input(f"Select a level for Bot {bot_num + 1} (1-13): ")
+
+    def re_ask_bot_level(self, bot_num):
+        return input(f"That's not valid. Select a level for Bot {bot_num + 1} (1-13): ")
+
+    def ask_games_amount(self):
+        return input("How many games? ")
+
+    def re_ask_games_amount(self):
+        return input("That's not a valid number. How many games?: ")
+
+    def ask_continue_or_leave(self, player_name):
+        pass
+
+    def re_ask_continue_or_leave(self, player_name):
+        pass
+
+    def ask_again(self):
+        pass
+
+    def ask_for_save(self):
+        return input("Do you want to save the game statistics? (Y/N): ").upper()
