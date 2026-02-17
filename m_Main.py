567eb49bce8c399770e3505d9afc432656343e51
Bogi168 <danielbogner5@gmail.com>
2026-02-17 04:11:51 +0100

    Update m_Main.py

--- m_Main.py
+++ m_Main.py
@@ -1,6 +1,6 @@
 from Main_Game.m_game import Game
+from Main_Game.m_Renderer import ConsoleRenderer
 
 if __name__ == "__main__":
-    game = Game()
+    game = Game(ConsoleRenderer)
     game.main()
-
